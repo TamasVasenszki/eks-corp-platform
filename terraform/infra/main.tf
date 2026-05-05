3acc21b51db9f3a67eed0cd570ae8cf293ccddcb
Tamas Vasenszki <vasitomikj@gmail.com>
2026-05-05 21:34:20 +0200

    feat: provision EKS cluster and managed node groups (multi-AZ, autoscaling)

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -9,3 +9,11 @@ module "ecr" {
   source       = "./modules/ecr"
   project_name = var.project_name
 }
+
+module "eks" {
+  source             = "./modules/eks"
+  project_name       = var.project_name
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  aws_region         = var.aws_region
+}
